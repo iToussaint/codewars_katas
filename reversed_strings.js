cc63f212d7f1e8bcaa5a235efd9e86f9cc86b92a
iToussaint <iradukundatoussaint01@gmail.com>
2025-07-06 19:31:38 +0200

    Reversed Strings

---
+++ reversed_strings.js
@@ -0,0 +1,12 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+Strings | Fundamentals
+*/
+
+function solution(str) {
+  return str.split("").reverse().join("");
+}
