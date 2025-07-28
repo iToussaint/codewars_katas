ec37ed81eb36b381dfca43172aa30549627d7612
iToussaint <iradukundatoussaint01@gmail.com>
2025-07-28 17:20:09 +0200

    Reverse List Order

--- break_camel_case.js
+++ break_camel_case.js
@@ -1,3 +1,14 @@
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+Strings | Fundamentals
+*/
+
 function solution(string) {
   return string
     .split("")
