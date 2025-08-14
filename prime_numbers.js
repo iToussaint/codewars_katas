1f7a1d58a51a27429a3708bcbba7f9ea3b8d680f
iToussaint <iradukundatoussaint01@gmail.com>
2025-08-14 13:31:38 +0200

    Prime Numbers

---
+++ prime_numbers.js
@@ -0,0 +1,48 @@
+/*
+Preface
+A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+A more detailed description of prime numbers can be found at: http://en.wikipedia.org/wiki/Prime_number
+
+The Problem
+You will need to create logic for the following two functions: isPrime(number) and getPrimes(start, finish)
+
+isPrime(number)
+Should return boolean true if prime, otherwise false
+
+getPrimes(start, finish)
+Should return a unique, sorted array of all primes in the range [start, finish] (i.e. both numbers inclusive). Note that this range can go both ways - e.g. getPrimes(10, 1) should return all primes from 1 to 10 both inclusive.
+
+Sample Input:
+isPrime(number):
+
+isPrime(0); // === false
+isPrime(1); // === false
+isPrime(2); // === true
+isPrime(3); // === true
+isPrime(4); // === false
+isPrime(5); // === true 
+getPrimes(start, finish):
+
+getPrimes(0, 0); // === []
+getPrimes(0, 30); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+getPrimes(30, 0); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+Algorithms
+*/
+
+function isPrime(number) {
+  return (
+    number > 1 &&
+    [...Array(Math.ceil(Math.sqrt(number))).keys()]
+      .map((num) => num + 1)
+      .filter((num) => num !== 1 && num !== number)
+      .every((num) => number % num !== 0)
+  );
+}
+
+function getPrimes(start, finish) {
+  return [...Array(Math.abs(finish - start) + 1).keys()]
+    .map((number) => number + (finish > start ? start : finish))
+    .filter((number) => isPrime(number));
+}
