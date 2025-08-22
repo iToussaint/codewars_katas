c6f5c64ef877e63c1f7881880fe4393782db544f
iToussaint <iradukundatoussaint01@gmail.com>
2025-08-22 15:06:03 +0200

    Factorial

---
+++ factorial2.js
@@ -0,0 +1,11 @@
+/*
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+
+Fundamentals
+*/
+
+function factorial(n) {
+  return n > 1 ? n * factorial(n - 1) : n === 0 ? 1 : n;
+}
